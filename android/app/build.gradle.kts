6c0f2ccdb86066b27fe30e7c0f66a9fc17328dcb
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2025-10-10 16:39:20 +0900

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
         release {
 
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
 }
